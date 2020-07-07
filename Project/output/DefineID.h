1de653e4a3755c83d1aec8608905ced13aeea5b7
zgh <hui6209822@163.com>
2020-07-07 15:44:13 +0800

    大彩4.3寸 VisualTFT工程文件

---
+++ Project/output/DefineID.h
@@ -0,0 +1,166 @@
+//创建画面Screen7的命令索引ID
+#define  _SCREEN_SCREEN7                                                       0
+
+//创建画面Screen0的命令索引ID
+#define  _SCREEN_SCREEN0                                                       1
+
+//创建画面Screen1的命令索引ID
+#define  _SCREEN_SCREEN1                                                       2
+
+//创建画面Screen2的命令索引ID
+#define  _SCREEN_SCREEN2                                                       3
+
+//创建画面Screen3的命令索引ID
+#define  _SCREEN_SCREEN3                                                       4
+
+//创建画面Screen4的命令索引ID
+#define  _SCREEN_SCREEN4                                                       5
+
+//创建画面Screen5的命令索引ID
+#define  _SCREEN_SCREEN5                                                       6
+
+//创建画面Screen6的命令索引ID
+#define  _SCREEN_SCREEN6                                                       7
+
+#define  _ANIMATION_SCREEN7_ANIMATION1                                         1
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY1                                       7
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY2                                       6
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY3                                      11
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY4                                      12
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY5                                      13
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY6                                      14
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY7                                      15
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY8                                      16
+
+//画面Screen0中按钮Button1按下时的图片
+#define  _IMG_SCREEN0_BUTTON1_DOWN                                             2
+
+#define  _BTN_SCREEN0_BUTTON1                                                  1
+
+//画面Screen0中按钮Button2按下时的图片
+#define  _IMG_SCREEN0_BUTTON2_DOWN                                             3
+
+#define  _BTN_SCREEN0_BUTTON2                                                  2
+
+//画面Screen0中按钮Button3按下时的图片
+#define  _IMG_SCREEN0_BUTTON3_DOWN                                             4
+
+#define  _BTN_SCREEN0_BUTTON3                                                  3
+
+//画面Screen0中按钮Button4按下时的图片
+#define  _IMG_SCREEN0_BUTTON4_DOWN                                             5
+
+#define  _BTN_SCREEN0_BUTTON4                                                  4
+
+//画面Screen0中按钮Button5按下时的图片
+#define  _IMG_SCREEN0_BUTTON5_DOWN                                             6
+
+#define  _BTN_SCREEN0_BUTTON5                                                  5
+
+//画面Screen0中按钮Button6按下时的图片
+#define  _IMG_SCREEN0_BUTTON6_DOWN                                             7
+
+#define  _BTN_SCREEN0_BUTTON6                                                  8
+
+//画面Screen0中按钮Button7按下时的图片
+#define  _IMG_SCREEN0_BUTTON7_DOWN                                             8
+
+#define  _BTN_SCREEN0_BUTTON7                                                  9
+
+//画面Screen0中按钮Button8按下时的图片
+#define  _IMG_SCREEN0_BUTTON8_DOWN                                             9
+
+#define  _BTN_SCREEN0_BUTTON8                                                 10
+
+#define  _PROGRESS_SCREEN0_PROGRESS1                                          17
+
+//画面Screen0中按钮Button9按下时的图片
+#define  _IMG_SCREEN0_BUTTON9_DOWN                                            11
+
+#define  _BTN_SCREEN0_BUTTON9                                                 19
+
+#define  _PROGRESS_SCREEN1_PROGRESS1                                           4
+
+#define  _PROGRESS_SCREEN1_PROGRESS2                                           5
+
+#define  _ANIMATION_SCREEN1_ICON1                                              9
+
+//画面Screen1中按钮Button1按下时的图片
+#define  _IMG_SCREEN1_BUTTON1_DOWN                                            14
+
+#define  _BTN_SCREEN1_BUTTON1                                                 10
+
+//画面Screen2中按钮Button1弹起时的图片
+#define  _IMG_SCREEN2_BUTTON1_UP                                              14
+
+//画面Screen2中按钮Button1按下时的图片
+#define  _IMG_SCREEN2_BUTTON1_DOWN                                            14
+
+#define  _BTN_SCREEN2_BUTTON1                                                 10
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY1                                       1
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY2                                       2
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY3                                       3
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY4                                       4
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY6                                       6
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY7                                       7
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY8                                       8
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY9                                       9
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY5                                       5
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY10                                     11
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY11                                     12
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY12                                     13
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY13                                     14
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY14                                     15
+
+#define  _TXT_DIS__SCREEN2_TEXT_DISPLAY15                                     16
+
+#define  _ANIMATION_SCREEN2_ICON1                                             17
+
+//画面Screen3中按钮Button1弹起时的图片
+#define  _IMG_SCREEN3_BUTTON1_UP                                              14
+
+//画面Screen3中按钮Button1按下时的图片
+#define  _IMG_SCREEN3_BUTTON1_DOWN                                            14
+
+#define  _BTN_SCREEN3_BUTTON1                                                 10
+
+//画面Screen4中按钮Button1弹起时的图片
+#define  _IMG_SCREEN4_BUTTON1_UP                                              14
+
+//画面Screen4中按钮Button1按下时的图片
+#define  _IMG_SCREEN4_BUTTON1_DOWN                                            14
+
+#define  _BTN_SCREEN4_BUTTON1                                                 10
+
+//画面Screen5中按钮Button1弹起时的图片
+#define  _IMG_SCREEN5_BUTTON1_UP                                              14
+
+//画面Screen5中按钮Button1按下时的图片
+#define  _IMG_SCREEN5_BUTTON1_DOWN                                            14
+
+#define  _BTN_SCREEN5_BUTTON1                                                 10
+
+#define  _BTN_SCREEN6_BUTTON1                                                  2
+
